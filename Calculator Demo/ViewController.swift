e9b8800427666e385a0872f6643ecf7f5aabec46
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-09-05 10:55:49 +0700

    Fixed the logic adding zeroes, floating point digit, and regular digits to the display so that there will be no leading zeros & only 1 floating point possible.

--- Calculator Demo/ViewController.swift
+++ Calculator Demo/ViewController.swift
@@ -19,16 +19,63 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - Stored Properties
+    
+    var isUserInTheMiddleOfTyping = false
 
     // MARK: - IBOutlet Properties
     
     @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var floatingPointButton: UIButton!
     
     // MARK: - IBAction Properties
     
     @IBAction func appendDigitButton(sender: UIButton) {
+        if isUserInTheMiddleOfTyping {
+            if self.displayLabel.text!.characters.first == "0" {
+                if sender.currentTitle == "0" && self.displayLabel.text!.characters.contains(".") {
+                    self.displayLabel.text! += sender.currentTitle!
+                }
+                else if self.displayLabel.text!.characters.contains(".") {
+                    self.displayLabel.text! += sender.currentTitle!
+                }
+                else if sender.currentTitle! == "." {
+                    self.floatingPointButton.enabled = false
+                    self.displayLabel.text! += sender.currentTitle!
+                }
+                else if sender.currentTitle == "0" {
+                    return
+                }
+                else {
+                    self.displayLabel.text!.removeAtIndex(self.displayLabel.text!.startIndex)
+                    self.displayLabel.text! += sender.currentTitle!
+                }
+            }
+            else {
+                if self.displayLabel.text!.characters.contains(".") {
+                    self.floatingPointButton.enabled = false
+                }
+                self.displayLabel.text! += sender.currentTitle!
+            }
+        }
+        else {
+            if sender.currentTitle! == "." {
+                self.displayLabel.text = "0."
+                self.floatingPointButton.enabled = false
+            }
+            else {
+                self.displayLabel.text = sender.currentTitle!
+            }
+            self.isUserInTheMiddleOfTyping = true
+        }
     }
     
+    @IBAction func clearDisplayButton(sender: UIButton) {
+        self.displayLabel.text = "0"
+        self.isUserInTheMiddleOfTyping = false
+        self.floatingPointButton.enabled = true
+    }
     
 }
 
